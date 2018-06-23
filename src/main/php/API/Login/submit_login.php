088d11cb06b9b34a4677cebe12acebe9fbc5c86c
Joshua "Shadow <ShadowLordAlpha@users.noreply.github.com>
2018-06-23 00:25:40 -0700

    Login Test
    - Removed test file
    + add basic files for login logic

---
+++ src/main/php/API/Login/submit_login.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Author: Josh "ShadowLordAlpha"
+ * File: submit_login.php
+ * Description: Validate the given credentials and sign in the user
+ */
+include_once('../include/common.php');
+
+debug('DEBUG', 'Getting arguments');
+$email = $_REQUEST['email'];
+$password = $_REQUEST['password'];
+
+debug('DEBUG', 'Validating required arguments');
+if($email=='') {
+    debug('DEBUG', 'Email empty, returning fail.');
+    echo json_encode(array('return_code'=>0, 'return_message'=>'Please enter Email.'));
+    exit();
+} else if($password=='') {
+    debug('DEBUG', 'Password empty, returning fail.');
+    echo json_encode(array('return_code'=>0, 'return_message'=>'Please enter Password.'));
+    exit();
+}
+
+debug('DEBUG', 'Running logic');
+// TODO: Actually code proper logic for this
+
+debug('DEBUG', 'Done');
+echo json_encode(array('return_code'=>1, 'return_message'=>'Success'));
+exit();
+?>
---
+++ src/main/php/include/common.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Author: Josh "ShadowLordAlpha"
+ * File: common.php
+ * Description: Contains useful and common functions or things that are normally included in the file
+ */
+set_time_limit(120);
+
+header('application/json');
+
+if($_REQUEST['debug']!='') {
+    header('text/plain');
+}
+
+function debug($tag, $message) {
+    if($_REQUEST['debug']!='') {
+        echo "[".date("Y-m-d H:i:s")."] $tag - $message"
+    }
+}
+?>
