74982a01014d475b81ff35960b37e61a6e92171c
Joshua "Shadow <ShadowLordAlpha@users.noreply.github.com>
2018-06-23 00:35:51 -0700

    change method name

--- src/main/php/API/Login/submit_login.php
+++ src/main/php/API/Login/submit_login.php
@@ -4,27 +4,27 @@
  * File: submit_login.php
  * Description: Validate the given credentials and sign in the user
  */
-include_once('../../../include/common.php');
+include_once('../../include/common.php');
 
-debug('DEBUG', 'Getting arguments');
+Debug('DEBUG', 'Getting arguments');
 $email = $_REQUEST['email'];
 $password = $_REQUEST['password'];
 
-debug('DEBUG', 'Validating required arguments');
+Debug('DEBUG', 'Validating required arguments');
 if($email=='') {
-    debug('DEBUG', 'Email empty, returning fail.');
+    Debug('DEBUG', 'Email empty, returning fail.');
     echo json_encode(array('return_code'=>0, 'return_message'=>'Please enter Email.'));
     exit();
 } else if($password=='') {
-    debug('DEBUG', 'Password empty, returning fail.');
+    Debug('DEBUG', 'Password empty, returning fail.');
     echo json_encode(array('return_code'=>0, 'return_message'=>'Please enter Password.'));
     exit();
 }
 
-debug('DEBUG', 'Running logic');
+Debug('DEBUG', 'Running logic');
 // TODO: Actually code proper logic for this
 
-debug('DEBUG', 'Done');
+Debug('DEBUG', 'Done');
 echo json_encode(array('return_code'=>1, 'return_message'=>'Success'));
 exit();
 ?>
