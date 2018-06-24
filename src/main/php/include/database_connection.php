c1f8452651cf41841e243929f7bb3daa84e172c6
Joshua "Shadow <ShadowLordAlpha@users.noreply.github.com>
2018-06-24 10:02:43 -0700

    hm

--- src/main/php/include/database_connection.php
+++ src/main/php/include/database_connection.php
@@ -3,10 +3,8 @@ $servername = $_SERVER['DATABASE_URL'];
 $username = $_SERVER['DATABASE_USER'];
 $password = $_SERVER['DATABASE_PASSWORD'];
 
-echo "$servername $username $password";
-
 // Create connection
-//$conn = mysqli_connect($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password);
 
 // Check connection
 if (!$conn) {
